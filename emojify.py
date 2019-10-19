ec3963157e7c15675d5889ade681adb6ed95afd0
Sam0017479 <32306564+Sam0017479@users.noreply.github.com>
2019-10-18 23:43:07 -0400

    Simplified character search.
    Switched regular if statement to regex.

--- emojify.py
+++ emojify.py
@@ -1,9 +1,11 @@
+import re
+
 def emojify(text):
     emojified = ""
 
     textList = list(text)
     for i in textList:
-        if i == "," or i == " " or i == "!" or i == "." or i == "'" or i == "?":
+        if re.search(r'[,". `~]', i):
             emojified += i
         else:
             emojified += f":regional_indicator_{i}: "
